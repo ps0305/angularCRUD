1ba7e4fb252698a2cf2f4c0bcc1bb3df3a294f76
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-04 01:01:05 +0530

    updateProduct component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,24 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HttpModule } from '@angular/http';
 import { ProductComponent } from './product/product.component';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { UpdateProductComponent } from './update-product/update-product.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ProductComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    UpdateProductComponent
+  ],
   imports: [
     BrowserModule,
-    HttpModule,FormsModule,
+    HttpModule,
+    FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
-      { path: 'product', component: ProductComponent }
+      { path: 'product', component: ProductComponent },
+      { path: 'updateProduct/:id', component: UpdateProductComponent }
     ])
   ],
   providers: [],
---
+++ src/app/update-product/update-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-product',
+  templateUrl: './update-product.component.html',
+  styleUrls: ['./update-product.component.css']
+})
+export class UpdateProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
