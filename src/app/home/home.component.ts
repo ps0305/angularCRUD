6297b920bd057329cd530624a096abca2720ffb1
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-04 00:53:39 +0530

    updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Http, Response, Headers } from '@angular/http';
 })
 export class HomeComponent implements OnInit {
   constructor(private http: Http) {}
+  id: number;
+  private headers = new Headers({ 'Content-Type': 'application/json' });
   products = [];
   fetchData = function() {
     this.http
@@ -14,6 +16,18 @@ export class HomeComponent implements OnInit {
       .subscribe((res: Response) => {
         this.products = res.json();
       });
+  };
+
+  deleteProduct = function(id) {
+    if (confirm('Are you sure?')) {
+      const url = `${'http://localhost:5555/products'}/${id}`;
+      return this.http
+        .delete(url, { headers: this.headers })
+        .toPromise()
+        .then(() => {
+          this.fetchData();
+        });
+    }
   };
 
   ngOnInit() {
