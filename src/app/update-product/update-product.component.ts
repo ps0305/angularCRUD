46ad59fe160f5d03fd157014808294202270f3c5
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-06 17:21:01 +0530

    update functionality

--- src/app/update-product/update-product.component.ts
+++ src/app/update-product/update-product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-update-product',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-product.component.css']
 })
 export class UpdateProductComponent implements OnInit {
+  id: number;
+  data: object = {};
+  products = [];
+  exist = false;
+  productObj: object = {};
+  private headers = new Headers({ 'Content-Type': 'application/json' });
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private http: Http
+  ) {}
 
+  updateProduct = function(product) {
+    this.productObj = {
+      p_id: product.p_id,
+      p_name: product.p_name,
+      p_cost: product.p_cost
+    };
+    const url = `${'http://localhost:5555/products'}/${this.id}`;
+    this.http
+      .put(url, JSON.stringify(this.productObj), { headers: this.headers })
+      .toPromise()
+      .then(() => {
+        this.router.navigate(['/']);
+      });
+  };
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = +params['id'];
+    });
+    this.http
+      .get('http://localhost:5555/products')
+      .subscribe((res: Response) => {
+        this.products = res.json();
+        for (var i = 0; i < this.products.length; i++) {
+          if (parseInt(this.products[i].id) === this.id) {
+            this.exist = true;
+            this.data = this.products[i];
+            break;
+          } else {
+            this.exist = false;
+          }
+        }
+      });
   }
-
 }
